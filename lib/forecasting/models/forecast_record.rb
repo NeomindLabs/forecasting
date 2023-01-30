dbb33775699dd2e600cca983396af19d421853ba
Jordan Owens <jkowens@gmail.com>
2023-01-30 12:32:14 -0500

    Refactor ForecastRecord::get

--- lib/forecasting/models/forecast_record.rb
+++ lib/forecasting/models/forecast_record.rb
@@ -18,8 +18,7 @@ module Forecasting
         forecast_client.delete(self)
       end
 
-      # It loads a new record from your Harvest account.
-      #
+      # It loads a new record from your Harvest account.  #
       # @return [Forecasting::Models::ForecastRecord]
       def fetch
         self.class.new(forecast_client.get(path), forecast_client: forecast_client)
@@ -43,7 +42,9 @@ module Forecasting
       #   instance
       def self.get(id, opts = {})
         client = opts[:forecast_client] || Forecasting::Client.new(**opts)
-        self.new({ 'id' => id }, opts).fetch
+        self.new({ 'id' => id }, opts).tap do |record|
+          record.attributes = client.get(record.path)
+        end
       end
     end
   end
